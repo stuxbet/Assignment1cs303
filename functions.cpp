85e3313df053e60e493878b435d6f21e2ef4ca31
stuxbet <123492671+stuxbet@users.noreply.github.com>
2024-02-05 17:26:51 -0600

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -13,7 +13,7 @@ int ReadTheDamnFile(int numList[], int counter){
 
         int value;
         while (iss >> value) {
-            
+            //increments counter when it addes value to array
             numList[counter++] = value;
         }
     }
@@ -22,6 +22,8 @@ int ReadTheDamnFile(int numList[], int counter){
 }
 
 int thisThangInHere(int Here[], int size, int target) {
+    //iterates through array to find target value
+    // returns index if successful and -1 if not
     for (int i = 0; i < size; ++i) {
         if (Here[i] == target) {
             return i;
@@ -32,9 +34,10 @@ int thisThangInHere(int Here[], int size, int target) {
 
 pair<int, int> modify(int index, int newval, int numList[]) {
     try {
+        // sets old value to the array at the selected index to save it
         int oldval = numList[index];
         numList[index] = newval;
-
+        //changes value and returns a pair of the new and old value
         pair<int, int> returnPair(newval, oldval);
         return returnPair;
     }
@@ -45,8 +48,9 @@ pair<int, int> modify(int index, int newval, int numList[]) {
 }
 
 void AddThatToEndOfArray(int arrayToModify[], int that, int &counter) {
-
+    
     try {
+        // adds value to last index of array using counter system
         arrayToModify[counter] = that;
         counter++;
     }
@@ -59,12 +63,14 @@ void AddThatToEndOfArray(int arrayToModify[], int that, int &counter) {
 void modifyArrayAtIndex(int arr[], int index, bool remove, int counter) {
     if (index >= 0 && index < counter) {
         if (remove) {
+            //if remove is selected we just set the array at that index to the next value
             for (int i = index; i < counter - 1; ++i) {
                 arr[i] = arr[i + 1];
             }
             counter--;
         }
         else {
+            // if remove isnt selected then we set it to 0
             arr[index] = 0;
         }
     }
